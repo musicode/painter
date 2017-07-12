b53e385c3f4d2d74cc1812ef7f45f7b52bd969db
musicode <musicode@qq.com>
2017-07-12 20:47:06 +0800

    update

--- src/function/drawActive.js
+++ src/function/drawActive.js
@@ -4,46 +4,49 @@
  */
 define(function () {
 
-  return function (shape, context) {
-
-    const thickness = 1
-    const size = 10
-
-    context.lineWidth = thickness
-    context.strokeStyle = '#E6E6E6'
-
-    let rect = shape.getRect()
-
+  function drawThumb(context, left, top, size) {
+    let gradient = context.createLinearGradient(left, top + size, left, top)
+    gradient.addColorStop(0, '#ddd')
+    gradient.addColorStop(1, '#f9f9f9')
     context.beginPath()
-    context.rect(rect.x, rect.y, rect.width, rect.height)
-    context.stroke()
-
-    let left = rect.x - size / 2
-    let center = rect.x + (rect.width - size) / 2
-    let right = rect.x + rect.width - size / 2
-
-    let top = rect.y - size / 2
-    let middle = rect.y + (rect.height - size) / 2
-    let bottom = rect.y + rect.height - size / 2
-
-    context.beginPath()
+    context.fillStyle = gradient
     context.strokeStyle = '#A2A2A2'
-    context.fillStyle = '#EEE'
-
     context.rect(left, top, size, size)
-    context.rect(center, top, size, size)
-    context.rect(right, top, size, size)
-    context.rect(right, middle, size, size)
-    context.rect(right, bottom, size, size)
-    context.rect(center, bottom, size, size)
-    context.rect(left, bottom, size, size)
-    context.rect(left, middle, size, size)
-
-
     context.stroke()
     context.fill()
+  }
 
+  return function (shape, context) {
 
+    const thumbSize = 12
+
+    context.lineWidth = 1
+    context.strokeStyle = '#DDD'
+
+    let { x, y, width, height } = shape.getRect()
+
+    // 矩形线框
+    context.beginPath()
+    context.rect(x, y, width, height)
+    context.stroke()
+
+    let left = x - thumbSize / 2
+    let center = x + (width - thumbSize) / 2
+    let right = x + width - thumbSize / 2
+
+    let top = y - thumbSize / 2
+    let middle = y + (height - thumbSize) / 2
+    let bottom = y + height - thumbSize / 2
+
+    // 周围的 8 个方块
+    drawThumb(context, left, top, thumbSize)
+    drawThumb(context, center, top, thumbSize)
+    drawThumb(context, right, top, thumbSize)
+    drawThumb(context, right, middle, thumbSize)
+    drawThumb(context, right, bottom, thumbSize)
+    drawThumb(context, center, bottom, thumbSize)
+    drawThumb(context, left, bottom, thumbSize)
+    drawThumb(context, left, middle, thumbSize)
 
   }
 
