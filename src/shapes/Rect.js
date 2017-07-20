3db101bf5f41f7c18cc7e22f66ce7faf445b0269
musicode <musicode@qq.com>
2017-07-20 14:59:39 +0800

    update

--- src/shapes/Rect.js
+++ src/shapes/Rect.js
@@ -88,6 +88,16 @@ define(function (require) {
       painter.fillRect(this.x, this.y, this.width, this.height)
     }
 
+    /**
+     * 正在绘制
+     *
+     * @param {Painter} painter
+     * @param {number} startX 起始点 x 坐标
+     * @param {number} startY 起始点 y 坐标
+     * @param {number} endX 结束点 x 坐标
+     * @param {number} endX 结束点 y 坐标
+     * @param {Function} 还原为鼠标按下时的画布
+     */
     drawing(painter, startX, startY, endX, endY, restore) {
 
       restore()
